f6aa09cc0ca6c5ce8d51c4d04484529914a90264
tbott <tbott@terpmail.umd.edu>
2024-02-28 21:12:58 -0500

    Added a new route on the app.py to the about-css.html page. Created the about-css.html page which is the same content-wise as the about.html page (plus a new paragraph about my dogs). It is also formatted using the style.css css file that I created.

--- app.py
+++ app.py
@@ -16,6 +16,10 @@ def hello():
 def about():
     return render_template('about.html')
 
+@app.route('/about-css')
+def aboutcss():
+    return render_template('about-css.html')
+
 if __name__ == '__main__':
     app.run()
 
